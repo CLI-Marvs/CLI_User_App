f5bcef95c43b65813f9831dd6530e557be90be0e
Rodfil T. Tayong <rttayong@cli.lan>
2024-08-28 16:32:13 +0800

    PushUpdates

--- app/Http/Controllers/ConcernController.php
+++ app/Http/Controllers/ConcernController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Jobs\ReplyFromAdminJob;
+use App\Mail\SendReplyFromAdmin;
 use App\Models\Concerns;
 use App\Models\InquiryLogs;
 use App\Models\Messages;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-
+use Illuminate\Support\Facades\Mail;
 
 class ConcernController extends Controller
 {
@@ -142,7 +143,8 @@ class ConcernController extends Controller
             $this->inquiryAdminLogs($request);
 
             ReplyFromAdminJob::dispatch($messages->ticket_id, $request->buyer_email, $messages->details_message, $message_id);
-            
+        /*   Mail::to($request->buyer_email)->send(new SendReplyFromAdmin($messages->ticket_id, $messages->details_message, $message_id)); */
+
 
             return response()->json("Sucessfully send");
         } catch (\Exception $e) {
