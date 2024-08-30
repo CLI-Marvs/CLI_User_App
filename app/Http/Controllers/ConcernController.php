3a6f29481437c87e2069b36ad13f1f0d6a4354af
Rodfil T. Tayong <rttayong@cli.lan>
2024-08-30 11:14:11 +0800

    PushUpdates

--- app/Http/Controllers/ConcernController.php
+++ app/Http/Controllers/ConcernController.php
@@ -204,19 +204,20 @@ class ConcernController extends Controller
     public function sendMessage(Request $request)
     {
         try {
+            $adminMessage = $request->details_message;
             $message_id = $request->message_id;
             $messages = new Messages();
             $messages->admin_id = $request->admin_id;
             $messages->admin_email = $request->admin_email;
             $messages->attachment = $request->attachment;
             $messages->ticket_id = $request->ticket_id;
-            $messages->details_message = $request->details_message;
+            $messages->details_message = $adminMessage;
             $messages->admin_name = $request->admin_name;
             $messages->save();
 
             $this->inquiryAdminLogs($request);
 
-            ReplyFromAdminJob::dispatch($messages->ticket_id, $request->buyer_email, $request->details_message, $message_id);
+            ReplyFromAdminJob::dispatch($messages->ticket_id, $request->buyer_email, $adminMessage, $message_id);
             /*   Mail::to($request->buyer_email)->send(new SendReplyFromAdmin($messages->ticket_id, $messages->details_message, $message_id)); */
 
 
