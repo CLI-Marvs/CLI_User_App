868352a49b44657df95e3b15b4fd756f03a656a3
Rodfil T. Tayong <rttayong@cli.lan>
2024-09-12 10:32:33 +0800

    PushUpdatse

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BIS</title>
+    <title>CLI</title>
     <link rel="icon" type="image/svg+xml" href="%PUBLIC_URL%/Images/CLI_Stacked.svg">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
