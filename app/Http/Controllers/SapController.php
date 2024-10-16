d0d021539f33c9353a002701194d48ad6715f306
Rodfil T. Tayong <rttayong@cli.lan>
2024-10-16 15:22:07 +0800

    PushUpdates

--- app/Http/Controllers/SapController.php
+++ app/Http/Controllers/SapController.php
@@ -22,28 +22,35 @@ class SapController extends Controller
 
     public function postInvoices(Request $request)
     {
-        $invoice = new Invoices();
-        $invoice->D_RECNNR = $request->input('contract_number');  
-        $invoice->D_BELNR = $request->input('document_number');  
-        $invoice->D_KUNNR = $request->input('customer_bp_number');  
-        $invoice->D_BUZEI = $request->input('document_number');  
-        $invoice->D_BUKRS = $request->input('sap_unique_id'); 
-        $invoice->D_SWENR = $request->input('company_name');  
-        $invoice->D_SGTXT = $request->input('project_code'); 
-        $invoice->D_NAME1 = $request->input('description'); 
-        $invoice->D_DMBTR = $request->input('customer_name'); 
-        $invoice->D_CPUDT = $request->input('invoice_amount');  
-        $invoice->D_ZFBDT = $request->input('entry_date'); 
-        $invoice->D_BUDAT = $request->input('due_date'); 
-       /*  $invoice->invoice_status = $request->input('invoice_status'); 
-        $invoice->status = $request->input('status');
-        $invoice->posting_response = $request->input('posting_response'); */
-    
-        $invoice->save();
-        return response()->json([
-            'message' => 'Invoice posted successfully',
-            'invoice' => $invoice
-        ]);
+        try {
+            \Log::info('Invoice posting request', [
+                'request' => $request->all()
+            ]);
+            $invoice = new Invoices();
+            $invoice->D_RECNNR = $request->input('contract_number');  
+            $invoice->D_BELNR = $request->input('document_number');  
+            $invoice->D_KUNNR = $request->input('customer_bp_number');  
+            $invoice->D_BUZEI = $request->input('document_number');  
+            $invoice->D_BUKRS = $request->input('sap_unique_id'); 
+            $invoice->D_SWENR = $request->input('company_name');  
+            $invoice->D_SGTXT = $request->input('project_code'); 
+            $invoice->D_NAME1 = $request->input('description'); 
+            $invoice->D_DMBTR = $request->input('customer_name'); 
+            $invoice->D_CPUDT = $request->input('invoice_amount');  
+            $invoice->D_ZFBDT = $request->input('entry_date'); 
+            $invoice->D_BUDAT = $request->input('due_date'); 
+           /*  $invoice->invoice_status = $request->input('invoice_status'); 
+            $invoice->status = $request->input('status');
+            $invoice->posting_response = $request->input('posting_response'); */
+        
+            $invoice->save();
+            return response()->json([
+                'message' => 'Invoice posted successfully',
+                'invoice' => $invoice
+            ]);
+        } catch (\Throwable $e) {
+            return response()->json(['message' => 'error.', 'error' => $e->getMessage()], 500);
+        }
     }
     
 }
