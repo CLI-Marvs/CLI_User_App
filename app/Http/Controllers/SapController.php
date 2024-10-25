c7124ccd9ac580ef3f697be7444a9261b3a9d1a8
Rodfil T. Tayong <rttayong@cli.lan>
2024-10-25 11:23:08 +0800

    PushUpdates

--- app/Http/Controllers/SapController.php
+++ app/Http/Controllers/SapController.php
@@ -95,7 +95,7 @@ class SapController extends Controller
                 $invoice->post_date = $request->input('D_BUDAT');
                 $invoice->customer_name = $request->input('D_NAME1');
                 $invoice->flow_type = $request->input('D_VBEWA');
-                $invoice->invoice_status = $request->input('D_STAT');
+                $invoice->status = $request->input('D_STATS');
                 /*  $invoice->invoice_status = $request->input('invoice_status'); 
                 $invoice->status = $request->input('status');
                 $invoice->posting_response = $request->input('posting_response'); */
