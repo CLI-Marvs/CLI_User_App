38a1613611cb84df002e70e6aab857b5b7bfa152
Kent A. Armelia <kaarmelia@gmail.com>
2024-11-06 10:52:19 +0800

    pushUpdates

--- resources/frontend/component/Alert.jsx
+++ resources/frontend/component/Alert.jsx
@@ -5,13 +5,13 @@ const Alert = ({ title = "Alert", onConfirm, onCancel, show }) => {
     if (!show) return null; // Only render if `show` is true
     return (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 overflow-auto">
-            <div className="bg-white p-[20px] rounded-[10px] shadow-custom5 w-[450px] min-h-[250px]">
+            <div className="bg-white p-[20px] rounded-[10px] shadow-custom5 w-[523px] min-h-[250px]">
                 <div className="p-[10px] flex flex-col gap-[26px]">
                     <div className="flex justify-center items-center">
                         <AiFillInfoCircle className="size-[37px] text-[#5B9BD5]" />
                     </div>
                     <h1 className="montserrat-medium text-[20px] text-center">{title}</h1>
-                    <div className="flex justify-end space-x-4 mt-10">
+                    <div className="flex justify-center items-center h-[55px] space-x-4 ">
                         <button
                             onClick={onCancel}
                             className="gradient-btn5 p-[1px] w-[92px] h-[35px] rounded-[10px]"
@@ -26,7 +26,7 @@ const Alert = ({ title = "Alert", onConfirm, onCancel, show }) => {
                             onClick={onConfirm}
                             className="gradient-btn5 w-[100px] h-[35px] rounded-[10px] text-sm text-white montserrat-semibold"
                         >
-                            Yes
+                            Confirm
                         </button>
                     </div>
                 </div>
