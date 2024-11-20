e719104f9a5d760d101f5e955f73755987b6e083
Kent A. Armelia <kaarmelia@gmail.com>
2024-11-20 09:51:36 +0800

    pushUpdates

--- resources/frontend/component/layout/loginpage/Login.jsx
+++ resources/frontend/component/layout/loginpage/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import CLILogo from "../../../../../public/Images/CLI20-Logo.png";
+import CLILogo from "../../../../../public/Images/CLI_Horizontal.svg";
 import GoogleLogo from "../../../../../public/Images/googleLogo.svg";
 import { API_PROVIDER } from "../../servicesApi/apiService";
 import { useLocation } from "react-router-dom";
@@ -40,16 +40,16 @@ const Login = () => {
             <div className="flex h-screen w-full relative overflow-hidden">
                 <div className="bg-left-side flex h-full w-1/2 "></div>
                 <div className="w-1/2 flex flex-col items-center justify-center p-4 z-2">
-                    <div className="">
-                        <img className="h-40" src={CLILogo} alt="cli logo" />
+                    <div className="mb-[26px]">
+                        <img className="w-[352px]" src={CLILogo} alt="cli logo" />
                     </div>
-                    <div className="bg-white px-12 pb-8 pt-12 rounded-xl shadow-custom w-108">
+                    <div className="bg-white shadow-custom w-[528px] h-[144px] rounded-[20px] py-[42px] px-[33px]">
                         <button
                             onClick={signinGoogle}
-                            className="relative flex justify-center items-center w-full text-sm border rounded-lg border-custom-lightgreen hover:shadow-custom3 text-custom-lightgreen h-11 mb-2"
+                            className="relative flex gap-[20px] justify-center items-center w-full h-[60px] text-[18px] border rounded-[10px] border-custom-lightgreen hover:shadow-custom3 text-custom-lightgreen"
                         >
-                            <img src={GoogleLogo} className="h-5 w-5 mr-6" />
-                            <span className="relative right-3 text-base">
+                            <img src={GoogleLogo} className="h-[30px] w-[30px]" />
+                            <span className="relative right-3">
                                 Login with Google
                             </span>
                         </button>
