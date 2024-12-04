8e6f06f4c376a7b1dac40545c7aa6c600d950aea
jah09 <jasuhot1@gmail.com>
2024-12-04 20:54:45 +0800

    feature/FE_BE-44

---
+++ resources/frontend/constant/data/allowedDepartment.js
@@ -0,0 +1,8 @@
+//Object that contains the allowed departments
+export const ALLOWED_DEPARTMENT = [
+    "Customer Relations - Services",
+    "Customer Relations - Turnovers",
+    "Accounts Management",
+    "Sales",
+    "Registration & Documentation",
+];
