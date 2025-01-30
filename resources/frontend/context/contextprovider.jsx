452b4fcbc086b658df261482af4a34862967ad5b
Kent A. Armelia <kaarmelia@gmail.com>
2025-01-30 08:53:27 +0800

    merging conflict 2

--- resources/frontend/context/contextprovider.jsx
+++ resources/frontend/context/contextprovider.jsx
@@ -127,10 +127,7 @@ export const ContextProvider = ({ children }) => {
         }
     };
 
-<<<<<<< HEAD
     
-=======
->>>>>>> b7aceb8e7b22620b301d83944a7462b7481c07e8
     const getAllConcerns = async () => {
         if (token) {
             setLoading(true);
