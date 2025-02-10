4078adc68df357fa8b85138fb63319f958d0d190
jah09 <jasuhot1@gmail.com>
2025-02-10 10:10:48 +0800

    feat: Added alert before updating for both edit user and edit department permissions

--- resources/frontend/component/layout/superadminpage/modals/UserModal/EditUserModal.jsx
+++ resources/frontend/component/layout/superadminpage/modals/UserModal/EditUserModal.jsx
@@ -1,23 +1,26 @@
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useEffect, useState, useMemo } from "react";
 import { showToast } from "@/util/toastUtil";
 import CircularProgress from "@mui/material/CircularProgress";
-import { normalizeData } from '@/component/layout/superadminpage/modals/UserModal/utils/normalizeData';
-import isEqual from 'lodash/isEqual';
-import Feature from '@/component/layout/superadminpage/component/Feature';
-import useFeature from '@/context/RoleManagement/FeatureContext';
-import useEmployeePermission from '@/context/RoleManagement/EmployeePermissionContext';
-import { employeePermissionService } from '@/component/servicesApi/apiCalls/roleManagement';
+import { normalizeData } from "@/component/layout/superadminpage/modals/UserModal/utils/normalizeData";
+import isEqual from "lodash/isEqual";
+import Feature from "@/component/layout/superadminpage/component/Feature";
+import useFeature from "@/context/RoleManagement/FeatureContext";
+import useEmployeePermission from "@/context/RoleManagement/EmployeePermissionContext";
+import { employeePermissionService } from "@/component/servicesApi/apiCalls/roleManagement";
+import Alert from "@/component/layout/mainComponent/Alert";
 
 const EditUserModal = ({ editEmployeeModalRef, selectedEmployee }) => {
     //States
-    const {  fetchEmployeeWithPermissions } = useEmployeePermission();
+    const { fetchEmployeeWithPermissions } = useEmployeePermission();
     const { features, fetchFeatures } = useFeature();
     const [isLoading, setIsLoading] = useState(false);
+    const [showAlert, setShowAlert] = useState(false);
     const [formData, setFormData] = useState({
         employee_id: 0,
         features: [],
     });
-    const [selectedEmployeeOldData, setSelectedEmployeeOldData] = useState(null); //Holds the old data of the selected department
+    const [selectedEmployeeOldData, setSelectedEmployeeOldData] =
+        useState(null); //Holds the old data of the selected department
 
     //Hooks
     useEffect(() => {
@@ -29,7 +32,7 @@ const EditUserModal = ({ editEmployeeModalRef, selectedEmployee }) => {
             setSelectedEmployeeOldData(selectedEmployee);
             fetchFeatures();
         }
-    }, [selectedEmployee])
+    }, [selectedEmployee]);
 
     //Event handler
     //Handle the permission change
@@ -50,7 +53,9 @@ const EditUserModal = ({ editEmployeeModalRef, selectedEmployee }) => {
         });
 
         // If the feature doesn't exist in formData, add it with the correct structure
-        const featureExists = updatedFeatures.some((feature) => feature.id === featureId);
+        const featureExists = updatedFeatures.some(
+            (feature) => feature.id === featureId
+        );
         if (!featureExists) {
             updatedFeatures.push({
                 id: featureId,
@@ -70,14 +75,17 @@ const EditUserModal = ({ editEmployeeModalRef, selectedEmployee }) => {
     };
 
     //Handle the submit/save button click
-    const handleSubmit = async () => {
+    const updateUserPermission = async () => {
         const payload = {
             employee_id: parseInt(formData.employee_id),
-            features: formData.features
+            features: formData.features,
         };
         try {
             setIsLoading(true);
-            const response = await employeePermissionService.editEmployeesWithPermissions(payload);
+            const response =
+                await employeePermissionService.editEmployeesWithPermissions(
+                    payload
+                );
 
             if (response.data?.statusCode === 200) {
                 showToast("Data updated successfully!", "success");
@@ -85,19 +93,18 @@ const EditUserModal = ({ editEmployeeModalRef, selectedEmployee }) => {
                     employee_id: 0,
                     features: [],
                 });
-            
+
                 if (editEmployeeModalRef.current) {
                     editEmployeeModalRef.current.close();
                 }
             }
-            await fetchEmployeeWithPermissions(true,false);
+            await fetchEmployeeWithPermissions(true, false);
         } catch (error) {
             console.log("Error updating user permission:", error);
         } finally {
             setIsLoading(false);
         }
-    }
-
+    };
 
     //Handle close the modal and cancel the modal
     const handleCloseModal = () => {
@@ -110,7 +117,7 @@ const EditUserModal = ({ editEmployeeModalRef, selectedEmployee }) => {
         if (editEmployeeModalRef.current) {
             editEmployeeModalRef.current.close();
         }
-    }
+    };
 
     //Function to check if the old data is the same as the new data, if YES, disable the button
     const isButtonDisabled = useMemo(() => {
@@ -121,9 +128,29 @@ const EditUserModal = ({ editEmployeeModalRef, selectedEmployee }) => {
         const oldDataNormalized = normalizeData(selectedEmployeeOldData);
         const newDataNormalized = normalizeData(formData);
 
-        // Compare old and new data  
+        // Compare old and new data
         return isEqual(oldDataNormalized, newDataNormalized);
     }, [selectedEmployeeOldData, formData]);
+
+    //Handle cancel the alert modal
+    const handleCancel = () => {
+        setShowAlert(false);
+    };
+
+    //Handle confirm the alert modal
+    const handleConfirm = () => {
+        updateUserPermission();
+        setShowAlert(false);
+        setTimeout(() => {
+            editEmployeeModalRef.current.close();
+        }, 1000);
+    };
+
+    //Handle show the alert modal
+    const handleShowUpdateAlert = () => {
+        setShowAlert(true);
+        modalRef.current.showModal();
+    };
 
     return (
         <dialog
@@ -131,17 +158,20 @@ const EditUserModal = ({ editEmployeeModalRef, selectedEmployee }) => {
             className="modal w-[683px] rounded-[10px] shadow-custom5 backdrop:bg-black/50  "
             ref={editEmployeeModalRef}
         >
-            <div className='relative p-[20px] mb-5 rounded-lg'>
-                <div className=''>
+            <div className="relative p-[20px] mb-5 rounded-lg">
+                <div className="">
                     <div>
-                        <button className="absolute top-3 right-3 w-10 h-10 items-center rounded-full bg-custombg3 text-custom-bluegreen hover:bg-custombg" onClick={handleCloseModal}>
+                        <button
+                            className="absolute top-3 right-3 w-10 h-10 items-center rounded-full bg-custombg3 text-custom-bluegreen hover:bg-custombg"
+                            onClick={handleCloseModal}
+                        >
                             ✕
                         </button>
                     </div>
                 </div>
-                <div className='flex flex-col gap-[36px] mt-[26px]'>
-                    <div className='w-full p-[10px] flex flex-col gap-[10px] relative mb-2'>
-                        <p className='text-sm font-semibold'>User</p>
+                <div className="flex flex-col gap-[36px] mt-[26px]">
+                    <div className="w-full p-[10px] flex flex-col gap-[10px] relative mb-2">
+                        <p className="text-sm font-semibold">User</p>
                         <div
                             className={`flex items-center border  rounded-[5px] overflow-hidden border-custom-bluegreen`}
                         >
@@ -151,9 +181,13 @@ const EditUserModal = ({ editEmployeeModalRef, selectedEmployee }) => {
                             <input
                                 name="name"
                                 type="text"
-                                value={selectedEmployee?.firstname + " " + selectedEmployee?.lastname || ''}
+                                value={
+                                    selectedEmployee?.firstname +
+                                        " " +
+                                        selectedEmployee?.lastname || ""
+                                }
                                 disabled
-                                className=' w-full px-4 text-sm focus:outline-none mobile:text-xs'
+                                className=" w-full px-4 text-sm focus:outline-none mobile:text-xs"
                             />
                         </div>
                         <div
@@ -166,14 +200,13 @@ const EditUserModal = ({ editEmployeeModalRef, selectedEmployee }) => {
                                 name="department"
                                 type="text"
                                 disabled
-                                value={selectedEmployee?.department || ''}
+                                value={selectedEmployee?.department || ""}
                                 className="w-full px-4 text-sm focus:outline-none mobile:text-xs"
                             />
                         </div>
-
                     </div>
-                    <div className='w-full p-[10px] flex flex-col gap-[10px]'>
-                        <p className='text-sm font-semibold'>Permissions</p>
+                    <div className="w-full p-[10px] flex flex-col gap-[10px]">
+                        <p className="text-sm font-semibold">Permissions</p>
                         {/*Display the features */}
                         {features &&
                             features.map((item, index) => (
@@ -182,9 +215,17 @@ const EditUserModal = ({ editEmployeeModalRef, selectedEmployee }) => {
                                     index={index}
                                     item={item}
                                     formData={formData}
-                                    handleFeaturePermissionChange={handleFeaturePermissionChange}
-                                    checkedExtractor={(item, permission, formData) =>
-                                        formData?.features?.find((feature) => feature.id === item.id)?.pivot?.[permission.value] || false
+                                    handleFeaturePermissionChange={
+                                        handleFeaturePermissionChange
+                                    }
+                                    checkedExtractor={(
+                                        item,
+                                        permission,
+                                        formData
+                                    ) =>
+                                        formData?.features?.find(
+                                            (feature) => feature.id === item.id
+                                        )?.pivot?.[permission.value] || false
                                     }
                                 />
                             ))}
@@ -204,17 +245,38 @@ const EditUserModal = ({ editEmployeeModalRef, selectedEmployee }) => {
                         </button>
                         <button
                             type="submit"
-                            onClick={handleSubmit}
+                            onClick={handleShowUpdateAlert}
                             disabled={isButtonDisabled || isLoading}
-                            className={`gradient-btn5 w-[100px] h-[35px] rounded-[10px] text-sm text-white montserrat-semibold ${isButtonDisabled || isLoading ? "cursor-not-allowed opacity-50" : ""}`}
+                            className={`gradient-btn5 w-[100px] h-[35px] rounded-[10px] text-sm text-white montserrat-semibold ${
+                                isButtonDisabled || isLoading
+                                    ? "cursor-not-allowed opacity-50"
+                                    : ""
+                            }`}
                         >
-                            {isLoading ? <CircularProgress className="spinnerSize" /> : "Save"}
+                            {isLoading ? (
+                                <CircularProgress className="spinnerSize" />
+                            ) : (
+                                "Save"
+                            )}
                         </button>
                     </div>
                 </div>
             </div>
+            <div>
+                <div className="">
+                    <Alert
+                        title="Are you sure you want to update this data?"
+                        show={showAlert}
+                        onCancel={handleCancel}
+                        onConfirm={handleConfirm}
+                        //You can pass onConfirm and onCancel props to customize the text of the buttons. Example below;
+                        // confirmText="Update"
+                        // cancelText="Cancel"
+                    />
+                </div>
+            </div>
         </dialog>
-    )
-}
+    );
+};
 
-export default EditUserModal
+export default EditUserModal;
