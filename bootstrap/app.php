c56decc795d17ccc6473a23374576113c3c2d1b3
jah09 <jasuhot1@gmail.com>
2025-03-31 15:22:19 +0800

    chore: Imlemented HSTS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\HstsMiddleware;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -15,7 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->append(HstsMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
