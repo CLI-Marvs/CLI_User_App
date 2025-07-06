7b5f74d7b86312acb5a07da0fb450d7ce3d8a377
Kent A. Armelia <kaarmelia@gmail.com>
2025-07-07 01:29:04 +0800

    hard reset to previous commit and change to accept incoming changes

--- resources/frontend/component/layout/surveyrelatedreportspage/SurveySummary.jsx
+++ resources/frontend/component/layout/surveyrelatedreportspage/SurveySummary.jsx
@@ -6,6 +6,8 @@ import apiService from '../../servicesApi/apiService';
 import SummaryLine from './surveyComponents/SummaryLine';
 import SummaryRating from './surveyComponents/SummaryRating';
 import SummaryTable from './surveyComponents/SummaryTable';
+import SummaryTextboxTable from './surveyComponents/SummaryTextboxTable';
+import SummaryVerticalBar from './surveyComponents/SummaryVerticalBar';
 const SurveySummary = () => {
 
     const { id } = useParams();
@@ -31,20 +33,43 @@ const SurveySummary = () => {
 
     useEffect(() => {
         const fetchRatingCounts = async () => {
-          try {
-            
-            const response = await apiService.get(`/experience-ratings/count/${surveyId}`);
-            setRatingCounts(response.data.data);
-          } catch (error) {
-            console.error('Error fetching rating counts:', error);
-          }
-        };
-      
-        if (id) {
-          fetchRatingCounts();
-        }
-      }, [id]);
+            try {
 
+                const response = await apiService.get(`/experience-ratings/count/${surveyId}`);
+                setRatingCounts(response.data.data);
+            } catch (error) {
+                console.error('Error fetching rating counts:', error);
+            }
+        };
+
+        if (id) {
+            fetchRatingCounts();
+        }
+    }, [id]);
+
+
+
+    /*  function groupQuestionsByOptions(questions) {
+         const groups = [];
+ 
+         questions.forEach((question) => {
+             const optionSignature = question.options.map(opt => opt.value).join('|');
+             let existingGroup = groups.find(group => group.signature === optionSignature);
+ 
+             if (existingGroup) {
+                 existingGroup.questions.push(question);
+             } else {
+                 groups.push({
+                     signature: optionSignature,
+                     questions: [question]
+                 });
+             }
+         });
+ 
+         return groups;
+     } */
+
+    /* const groupedTables = surveySummary?.questions ? groupQuestionsByOptions(surveySummary.questions) : []; */
 
 
       function normalizeOptions(question) {
@@ -135,14 +160,24 @@ const SurveySummary = () => {
                 {surveySummary?.questions?.map((item, index) => {
                     return (
                         <div key={index}>
-                            {item?.options?.length > 5 ? (
-                                <SummaryLine question={item} />
-                            ) : (
-                                <SummaryBar question={item} />
+                            {item?.input_type === "multiple-choice" && (
+                                item?.options?.length > 5 ? (
+                                    <SummaryLine question={item} />
+                                ) : (
+                                    <SummaryBar question={item} />
+                                )
                             )}
+                            {item?.input_type === "textbox" && (
+                               <SummaryTextboxTable question={item} />
+                            )}
+                            {item?.input_type === "checkboxes" && (
+                                <SummaryVerticalBar question={item} />
+                            )}
+                            
                         </div>
                     );
                 })}
+               
             </div>
         </div>
     )
