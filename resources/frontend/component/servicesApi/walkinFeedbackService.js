723bfc58846e9c0f6a83620292fb1fef108f3c28
jah09 <jasuhot1@gmail.com>
2025-07-07 13:49:15 +0800

    refactor: streamline authorization handling in walkinFeedbackService

--- resources/frontend/component/servicesApi/walkinFeedbackService.js
+++ resources/frontend/component/servicesApi/walkinFeedbackService.js
@@ -1,29 +1,27 @@
 import axios from "axios";
 
 const WALKIN_FEEDBACK_BASE_URL = import.meta.env.VITE_FEEDBACK_API_URL;
+const username = import.meta.env.VITE_AUTH_WALKIN_USERNAME;
+const password = import.meta.env.VITE_AUTH_WALKIN_PASSWORD;
+const basicAuth = btoa(`${username}:${password}`);
 
 const walkinFeedbackService = axios.create({
     baseURL: `${WALKIN_FEEDBACK_BASE_URL}/api/v1/emoji-walkin`,
     headers: {
         "Content-Type": "application/json",
+        Authorization: `Basic ${basicAuth}`,
     },
 });
 
 walkinFeedbackService.interceptors.request.use((config) => {
-    // const csrfToken = document.querySelector('meta[name="csrf-token"]');
-    // const token = localStorage.getItem("authToken");
     const csrfToken = document.querySelector('meta[name="csrf-token"]');
-    const userToken = localStorage.getItem("authToken");
     if (csrfToken) {
         const token = csrfToken.getAttribute("content");
         if (token) {
             config.headers["X-CSRF-TOKEN"] = token;
         }
-
-        if (userToken) {
-            config.headers["Authorization"] = `Bearer ${userToken}`;
-        }
     }
+
     return config;
 });
 
