91b5a4456aeceff47f691fde537150f2b43baa57
Kent A. Armelia <kaarmelia@gmail.com>
2025-07-22 18:40:19 +0800

    CEXP-27 fix: resolving submenu ui overlap

--- resources/frontend/component/App.jsx
+++ resources/frontend/component/App.jsx
@@ -115,7 +115,7 @@ const App = () => {
         return (
             <div className="bg-white relative max-h-screen flex flex-col h-screen">
                 <Navbar />
-                <div className="flex flex-1 overflow-hidden z-30">
+                <div className="relative flex flex-1 overflow-hidden z-30">
                     <Sidebar />
                     <div className="flex-1 overflow-y-auto bg-custom-grayFA z-20">
                         <Outlet />
