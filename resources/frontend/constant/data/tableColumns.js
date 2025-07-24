280cd178e9bb014778f90edc60d4e13c352e19ce
jah09 <jasuhot1@gmail.com>
2025-07-24 11:11:30 +0800

    style: change text labels for clarity in WalkinListPage and WalkinTransactionHistoryPage; update table column label from "Inquiry Type" to "Category Type".

--- resources/frontend/constant/data/tableColumns.js
+++ resources/frontend/constant/data/tableColumns.js
@@ -29,7 +29,7 @@ export const WALKIN_COLUMNS = [
 export const WALKIN_HISTORY_COLUMNS = [
     { label: "Full Name", width: "w-[250px]" },
     { label: "Priority Number", width: "w-[0px]" },
-    { label: "Inquiry Type", width: "w-[300px]" },
+    { label: "Category", width: "w-[300px]" },
     { label: "Status", width: "w-[200px]" },
     { label: "Date", width: "w-[150px]" },
 ];
